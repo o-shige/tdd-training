0ba44f5bad6206cf2319895d2d7188fff59774d2
o-shige <create.method@gmail.com>
2025-11-23 20:34:46 +0900

    コード修正

--- backend/app/domain/jwt.py
+++ backend/app/domain/jwt.py
@@ -14,8 +14,8 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     to_encode = data.copy()
     if expires_delta is None:
         expires_delta = timedelta(minutes=DEFAULT_EXPIRE_MINUTES)
-        expire = datetime.now(timezone.utc) + expires_delta
-        to_encode.update({"exp": expire})
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
